698189e5d0291518f22063a5210a84425812f76d
Ahmed Raza <rahme1758@gmail.com>
2023-11-18 13:42:45 +0500

    Now server is run of Hello World

--- server.mjs
+++ server.mjs
@@ -2,10 +2,14 @@ console.log("this is express server")
 
 import express from 'express';
 const app = express ()
-const port = 3000
+const port = 3006
 
 app.get('/',(req, res) => {
-    res.send('Hello World')
+    res.send('Hello World' + new Date())
+})
+
+app.get('/profile',(req, res) => {
+    res.send('This is profile'   +  new Date())
 })
 
 app.listen(port, () => {
